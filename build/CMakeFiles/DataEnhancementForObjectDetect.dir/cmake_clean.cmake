2983023082206b86bf9277286582d08b7878292f
misaka14869 <2422219873@qq.com>
2023-12-29 15:28:33 +0800

    CMake Init

---
+++ build/CMakeFiles/DataEnhancementForObjectDetect.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataEnhancementForObjectDetect.dir/main.cpp.obj"
+  "CMakeFiles/DataEnhancementForObjectDetect.dir/main.cpp.obj.d"
+  "D:/code/GitProjects/DataEnhancementForObjectDetect/bin/DataEnhancementForObjectDetect.exe"
+  "D:/code/GitProjects/DataEnhancementForObjectDetect/bin/DataEnhancementForObjectDetect.exe.manifest"
+  "D:/code/GitProjects/DataEnhancementForObjectDetect/bin/DataEnhancementForObjectDetect.pdb"
+  "D:/code/GitProjects/DataEnhancementForObjectDetect/bin/libDataEnhancementForObjectDetect.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataEnhancementForObjectDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
